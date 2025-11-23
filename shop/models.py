6e9e0b9101471cb3c85d8baaa56df41f10fbddf9
AA <arshanov8807@mail.ru>
2025-11-23 22:51:05 +0300

    Добавлена модель PromoCode и файлы-фикстуры yaml для инициализации БД

--- shop/models.py
+++ shop/models.py
@@ -10,3 +10,15 @@ class Purchase(models.Model):
     person = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
     date = models.DateTimeField(auto_now_add=True)
+    promo_code = models.ForeignKey('shop.PromoCode', on_delete=models.PROTECT, null=True, blank=True)
+
+class PromoCode(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name="Промокод")
+    discount_percent = models.DecimalField(
+        max_digits=2, decimal_places=0,
+        verbose_name="Скидка в процентах",
+        help_text="Значение скидки в процентах (например, 15)"
+    )
+
+    def __str__(self):
+        return f"{self.name} - {self.discount_percent}%"
